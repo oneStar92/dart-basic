c3cfa155121d03fb5571766324d298b5c75bb672
IL Seong Yoon <onestar7984@gmail.com>
2023-11-30 16:58:00 +0900

    refactor: pray 메서든 구현(Cleric)

--- lib/231130/231130_task.dart
+++ lib/231130/231130_task.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Cleric {
   final String name;
   int hp = 50;
@@ -11,6 +13,20 @@ class Cleric {
     if (mp >= 5) {
       mp -= 5;
       hp = maximumHp;
+    }
+  }
+
+  int pray(int second) {
+    final recoverableMP = maximumMp - mp;
+    if (recoverableMP == 0) {
+      return recoverableMP;
+    } else if (recoverableMP < second) {
+      return recoverableMP;
+    } else if (recoverableMP == second) {
+      return recoverableMP;
+    } else {
+      final recoveringMP = second + Random().nextInt(3);
+      return recoverableMP < recoveringMP ? recoverableMP : recoveringMP;
     }
   }
 }
