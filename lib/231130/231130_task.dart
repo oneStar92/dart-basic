a55d6ac9f59c0bc56414dcad56ad3988b59a8684
IL Seong Yoon <onestar7984@gmail.com>
2023-12-01 14:27:26 +0900

    refactor: Cleric Type maximumHp, maximumMp static 키워드 추가

--- lib/231130/231130_task.dart
+++ lib/231130/231130_task.dart
@@ -1,11 +1,11 @@
 import 'dart:math';
 
 class Cleric {
+  static final int maximumHp = 50;
+  static final int maximumMp = 50;
   final String name;
   int hp = 50;
-  final int maximumHp = 50;
   int mp = 50;
-  final int maximumMp = 50;
 
   Cleric(this.name);
 
