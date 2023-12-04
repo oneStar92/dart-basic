1ec6f63da7b282ab9595b8c93f1aa5ea6f4fb56f
IL Seong Yoon <onestar7984@gmail.com>
2023-12-04 14:32:40 +0900

    refactor: Wand Type name Setter 구현 및 _name 캡슐화

--- lib/231204/Wand.dart
+++ lib/231204/Wand.dart
@@ -1,9 +1,17 @@
 class Wand {
-  String name;
+  String _name;
   double power;
 
+  set name(String name) {
+    if (name != null && name.length >= 3) {
+      _name = name;
+    } else {
+      Exception("Invalid name value");
+    }
+  }
+
   Wand({
-    required this.name,
+    required name,
     required this.power,
-  });
+  }) : _name = name;
 }
