9a6a974ddefe505e5f1bacadea2d4ee70e105bf3
IL Seong Yoon <onestar7984@gmail.com>
2023-12-04 15:12:35 +0900

    feat: solved 2-3

--- lib/231204/Collection_Task_2.dart
+++ lib/231204/Collection_Task_2.dart
@@ -20,4 +20,19 @@ void main() {
   }
 
   people.forEach((person) => print(person.name));
+
+  final people2 = <Person, int>{
+    Person(name: '홍길동'): 20,
+    Person(name: '한석봉'): 25,
+  };
+
+  for (final e in people2.entries) {
+    print('${e.key.name}의 나이는 ${e.value}살');
+  }
+
+  for (final p in people2.keys) {
+    print('${p.name}의 나이는 ${people2[p]}살');
+  }
+
+  people2.forEach((person, age) => print('${person.name}의 나이는 $age살'));
 }
