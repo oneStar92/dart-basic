5702bc057fb9f5519cc624e1558cc761ce81693d
IL Seong Yoon <onestar7984@gmail.com>
2023-12-04 14:35:29 +0900

    refactor: Wizard Type mp 변수 추가

--- lib/231204/Wizard.dart
+++ lib/231204/Wizard.dart
@@ -3,11 +3,13 @@ import 'Wand.dart';
 class Wizard {
   String name;
   int hp;
+  int mp;
   Wand wand;
 
   Wizard({
     required this.name,
     required this.hp,
+    required this.mp,
     required this.wand,
   });
 }
