dd6911fa013cddb56e7bdabb53f4dd0ec02af3fe
IL Seong Yoon <onestar7984@gmail.com>
2023-12-05 15:28:29 +0900

    feat: TangibleAsset Abstract Type 구현

---
+++ lib/231205/231205_TangibleAsset.dart
@@ -0,0 +1,11 @@
+abstract class TangibleAsset {
+  final String name;
+  final int price;
+  final String color;
+
+  TangibleAsset({
+    required this.name,
+    required this.price,
+    required this.color
+  });
+}
