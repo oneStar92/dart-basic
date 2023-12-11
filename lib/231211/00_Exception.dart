86e62ad7b54ae3a43fea5e4c3b3e8dd78616507f
IL Seong Yoon <onestar7984@gmail.com>
2023-12-11 16:32:21 +0900

    refactor: Enum Exception Test

--- lib/231211/00_Exception.dart
+++ lib/231211/00_Exception.dart
@@ -1,3 +1,32 @@
+class InvalidTypeCasting implements Exception {
+  String message = '잘못된 타입 캐스팅 입니다.';
+}
+
+class InvalidData implements Exception {
+  String message = '잘못된 데이터 입니다.';
+}
+
+enum AppError {
+  invalidTypeCasting,
+  invalidData,
+  unknown;
+
+  Exception get error {
+    switch (this) {
+      case AppError.invalidTypeCasting:
+        return InvalidTypeCasting();
+      case AppError.invalidData:
+        return InvalidData();
+      case AppError.unknown:
+        return Exception();
+    }
+  }
+}
+
+void some() {
+  throw AppError.invalidTypeCasting.error;
+}
+
 void main() {
   final numString = '10.5';
   int num;
@@ -9,4 +38,12 @@ void main() {
   }
 
   print(num);
+
+  try {
+    some();
+  } on InvalidTypeCasting catch (e) {
+    print(e.message);
+  } catch (e) {
+    print(e);
+  }
 }
