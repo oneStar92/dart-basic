8ec08a0de6c484ca1f3df05e1f56cf8c25ac55d9
IL Seong Yoon <onestar7984@gmail.com>
2023-12-13 17:17:15 +0900

    feat: 비동기 File 처리 Solv

---
+++ lib/231213/00_FileAsync.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+Future<String> readFile(String path) async => await File(path).readAsString();
+Future<void> writeFile(String path, String string) async => await File(path).writeAsString(string);
+
+void main() {
+  /*
+  File('sample.csv').readAsString()
+      .then((value) => value.replaceAll('한석봉', '이석봉'))
+      .then((value) => File('sample_copy.csv').writeAsString(value))
+      .catchError((err) => print('Invalid Path'));
+   */
+
+  readFile('sample.csv')
+      .then((value) => value.replaceAll('한석봉', '김석봉'))
+      .then((value) => writeFile('sample_copy.csv', value))
+      .catchError((err) => print('Invalid Path'));
+}
